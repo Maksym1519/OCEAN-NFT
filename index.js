bfd613ce14d32c7cb48db1cbea26609d55efb164
Maksym1519 <72689882+Maksym1519@users.noreply.github.com>
2022-11-09 10:13:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,10 +4,36 @@ const sliderBox = document.querySelector('.top-NFT_body');
 document.querySelector('.slider-next').addEventListener('click', function() {
   offset = offset + 330;
   sliderBox.style.left = -offset + 'px';
-  
-} )
+  sliderBox.style.overflow = 'visible';
+  } )
 document.querySelector('.slider-prev').addEventListener('click', function() {
   offset = offset - 330;
   sliderBox.style.left = -offset + 'px';
-  
 } )
+document.querySelector('.top-NFT_body').addEventListener('click', function() {
+  sliderBox.style.overflow = 'scroll'
+})
+/*-----------------------------------------------------------------------*/
+const header = document.querySelector('._navigation-hover');
+const headerLinks = document.querySelector('.gamburger-hover');
+
+/*------------------------------------------------------------------------*/
+(function() {
+ const burger = document.querySelector('.header-gamburger')
+ burger.addEventListener('click', () => {
+  offset = offset + 600;
+  header.style.left = -offset + 'px';
+  headerLinks.style.height = '200px'
+  })
+}())
+/*-------------------------------------------------------------------------------- */
+
+/*--------------------------------------------------------------------------------*/
+const burgerClose = document.querySelector('.gamburger-close')
+burgerClose.addEventListener('click', () => {
+  offset = offset -600
+  header.style.left = -offset + 'px';
+  headerLinks.style.height = '0px'
+})
+
+
